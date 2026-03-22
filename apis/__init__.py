6c4708046753b97c631a8da23a608dc2b8b2ed08
ByteFlow <fakeshadow1337@gmail.com>
2026-03-22 16:07:34 +0800

    refactor: reorganize TPLinkAPI and get_devices functionality into routers directory

--- apis/__init__.py
+++ apis/__init__.py
@@ -1,3 +1,4 @@
-from .tplink_api import TPLinkAPI
+from .routers.tplink.tplink_api import TPLinkAPI
+from .routers.tplink.get_devices import tplink_get_devices
 from .check_isp import check_isp
 from .get_gateway import get_gateway_ip
