305c823e6e6e49f25e598e32dd2b9ee6ae39f417
ByteFlow <fakeshadow1337@gmail.com>
2026-03-25 15:08:02 +0800

    refactor: streamline imports in __init__.py and test files

--- src/autodialer/__init__.py
+++ src/autodialer/__init__.py
@@ -2,3 +2,18 @@
 __version__ = "0.1.0"
 
 from .apis.check_isp import check_isp, check_isp_with_retries
+from .apis.get_gateway import (get_gateway_ip_on_linux,
+                                get_gateway_ip_on_unix, 
+                                get_gateway_ip_on_windows)
+from .apis.routers.tplink.tplink_api import TPLinkAPI
+from .pppoe import is_target_asn
+
+__all__ = [
+    "check_isp",
+    "check_isp_with_retries",
+    "get_gateway_ip_on_linux",
+    "get_gateway_ip_on_unix",
+    "get_gateway_ip_on_windows",
+    "TPLinkAPI",
+    "is_target_asn",
+]
