5abac1bc365c8bdd0e8333030e66f28e6fd1f7a4
ByteFlow <fakeshadow1337@gmail.com>
2026-05-01 20:05:38 +0800

    fix(reconnection): remove error logging for unknown arguments in main function

--- src/autodialer/reconnection.py
+++ src/autodialer/reconnection.py
@@ -125,17 +125,6 @@ class Reconnection:
                 self.run_reconnection(mode="asn", asn=argv[2])
             case "-c" | "--change":
                 self.run_reconnection(mode="change", asn=None)
-            case _:
-                logger.error("Unknown argument: %s", argv[1])
-                if Path(argv[0]).suffix.lower() == ".py":
-                    logger.error(
-                        "Usage: python reconnection.py [-f|--force] [-a|--asn <ASN>] [-c|--change]"
-                    )
-                else:
-                    logger.error(
-                        "Usage: autodialer [-f|--force] [-a|--asn <ASN>] [-c|--change]"
-                    )
-                exit(1)
 
 
 def main():
