c1c8aff34e0a6267c0d06be1229705b4172becf6
ByteFlow <fakeshadow1337@gmail.com>
2026-05-15 15:07:35 +0800

    test: improve assertion for non-TTY device table output

--- tests/test_get_devices.py
+++ tests/test_get_devices.py
@@ -41,6 +41,6 @@ class TestGetDevices(unittest.TestCase):
                 from autodialer.get_devices import print_devices_table
 
                 print_devices_table(devices)
-                self.assertNotIn(
-                    "\033", mock_print.call_args[0][0]
+                self.assertFalse(
+                    any("\033" in arg for arg in mock_print.call_args[0])
                 )  # Check that no ANSI color codes are present
