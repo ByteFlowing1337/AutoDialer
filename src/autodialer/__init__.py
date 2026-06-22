49268c74ccb5dac4ef435ae3c5c41034bba38453
Ivy Xu <fakeshadow1337@gmail.com>
2026-06-22 20:01:46 +0800

    Add TYPE_CHECKING to __init__.py

--- src/autodialer/__init__.py
+++ src/autodialer/__init__.py
@@ -1,11 +1,13 @@
 __version__ = "0.4.0"
 __author__ = "Byteflow"
 
+from typing import TYPE_CHECKING
 
-if False:
+if TYPE_CHECKING:
     from autodialer.get_devices import get_devices, print_devices_table
+    from autodialer.network import get_router
     from autodialer.reconnection import reconnect
-    from autodialer.routers import RouterAPI, get_router
+    from autodialer.routers import RouterAPI
 
 __all__ = ["get_devices", "print_devices_table", "reconnect", "RouterAPI", "get_router"]
 
@@ -32,7 +34,3 @@ def __getattr__(name):
 
         return get_router
     raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
-
-
-def __dir__():
-    return sorted(set(globals().keys()) | set(__all__))
